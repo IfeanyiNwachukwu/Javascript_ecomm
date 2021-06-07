241f2cfd8635845f9a226acc6eac6e2a176a1335
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-06-07 02:00:32 +0100

    adding a middleware for requiring authentication to visit the admin page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const multer = require('multer');
 
-const {handleErrors}  = require('./middlewares')
+const {handleErrors,requireAuth}  = require('./middlewares')
 const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -13,23 +13,29 @@ const upload = multer({storage: multer.memoryStorage()});
 
 
 
-
-
-
-router.get('/admin/products',async (req,res) => {
+router.get('/admin/products',requireAuth, async (req,res) => {
+    if(req.session.userId){
+        return res.redirect('/signin');
+    }
     const products = await productsRepo.GetAll();
     res.send(productsIndexTemplate({products}));
 });
 
-router.get('/admin/products/new',(req,res) => {
+router.get('/admin/products/new',requireAuth, (req,res) => {
+    if(req.session.userId){
+        return res.redirect('/signin');
+    }
     res.send(newProductsTemplate({}));
 });
 
 
 
-router.post('/admin/products/new',upload.single('image'),[requireTitle,requirePrice],
+router.post('/admin/products/new',requireAuth,upload.single('image'),[requireTitle,requirePrice],
 handleErrors(newProductsTemplate),
  async(req,res) => {
+    if(req.session.userId){
+        return res.redirect('/signin');
+    }
    
     const image = req.file.buffer.toString('base64');
     const {title,price} = req.body;
